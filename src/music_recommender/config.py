035d77b55900110a158e5553e95e57db915a330f
Moad26 <moad2632005@gmail.com>
2025-10-12 10:28:32 +0100

    testing overlap between spotify vs fma-small dataset
    didn't go well ,yet

--- src/music_recommender/config.py
+++ src/music_recommender/config.py
@@ -1,3 +1,33 @@
 from pathlib import Path
 
-class datasetConfig:
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
+
+
+class PathConfig(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="PATH_")
+    root: Path = PROJECT_ROOT
+    data: Path = Field(default_factory=lambda: PROJECT_ROOT / "data")
+    models: Path = Field(default_factory=lambda: PROJECT_ROOT / "models")
+    logs: Path = Field(default_factory=lambda: PROJECT_ROOT / "logs")
+    fma_metadata: Path = Field(
+        default_factory=lambda: PROJECT_ROOT / "data" / "raw" / "fma_metadata"
+    )
+    fma_small: Path = Field(
+        default_factory=lambda: PROJECT_ROOT / "data" / "raw" / "fma_small"
+    )
+    spotify: Path = Field(
+        default_factory=lambda: PROJECT_ROOT / "data" / "raw" / "spotify-12m-songs"
+    )
+
+    def model_post_init(self, __context):
+        self.models.mkdir(exist_ok=True, parents=True)
+        self.logs.mkdir(exist_ok=True, parents=True)
+
+
+class Config(BaseSettings):
+    model_config = SettingsConfigDict(env_nested_delimiter="__")
+
+    paths: PathConfig = Field(default_factory=lambda: PathConfig())
